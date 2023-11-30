bd489ad86e116abf32d6a9b53242722b7791d948
Ed Davey <ed@dxw.com>
2023-11-30 18:21:35 +0000

    Configure environments and define dependencies
    We have the 2 profiles:
    
    - local: to run against your local development machine
    - deployed: to run against a deployed instance of the app

---
+++ playwright.config.ts
@@ -0,0 +1,47 @@
+import 'dotenv/config'
+import { defineConfig, devices } from '@playwright/test'
+
+export default defineConfig({
+  testDir: './tests',
+  fullyParallel: false,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  maxFailures: 1,
+  workers: 1,
+  reporter: 'html',
+  timeout: process.env.CI ? 5 * 60 * 1000 : 2 * 60 * 1000,
+  use: {
+    trace: 'on-first-retry',
+    video: 'on-first-retry',
+  },
+  projects: [
+    {
+      name: 'setupDeployed',
+      testMatch: /.*\.setup\.ts/,
+      use: { baseURL: process.env.RUBY_IMPLEMENTATION_DEPLOYED_URL },
+    },
+    {
+      name: 'deployed',
+      use: {
+        ...devices['Desktop Chrome'],
+        baseURL: process.env.RUBY_IMPLEMENTATION_DEPLOYED_URL,
+      },
+      dependencies: ['setupDeployed'],
+    },
+    {
+      name: 'setupLocal',
+      testMatch: /.*\.setup\.ts/,
+      use: {
+        baseURL: process.env.RUBY_IMPLEMENTATION_LOCAL_URL,
+      },
+    },
+    {
+      name: 'local',
+      use: {
+        ...devices['Desktop Chrome'],
+        baseURL: process.env.RUBY_IMPLEMENTATION_LOCAL_URL,
+      },
+      dependencies: ['setupLocal'],
+    },
+  ],
+})
